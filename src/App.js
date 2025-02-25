ad921bb35a921f3aa242de1ea6c753ffbe76f2c8
annesura <annesura00@gmail.com>
2025-02-25 13:09:09 -0500

    Final touches

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-        <h1>Dictionary</h1>
+        <h1 className="main">Dictionary</h1>
         <Dictionary defaultKeyword="eclipse" />
       </header>
       <footer>This project is coded by Anne Sura, and is open-sourced on <a href="https://github.com/annesura/react-dictionary-project" target="_blank" rel="noreferrer" >GitHub</a>.</footer>
