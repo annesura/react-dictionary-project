e93a61df7db10b2cadd3a74b71e2640612fb3041
annesura <annesura00@gmail.com>
2025-02-25 12:50:45 -0500

    Fixed missing prop

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props) {
                 {props.photos.slice(3).map(function(photo, index) {
                     return (
                     <div className="col-4 mt-2 mb-2" key={index}>
-                        <a href={photo.src.original} target="_blank" rel="noreferrer">
+                        <a href={photo.src.original} alt={photo.src.alt} target="_blank" rel="noreferrer">
                             <img src={photo.src.landscape} className="img-fluid" />
                         </a>
                     </div> );
