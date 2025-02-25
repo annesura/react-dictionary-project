fe8f48c4796ab59d28a8d485ccf9f9c0f00d91e3
annesura <annesura00@gmail.com>
2025-02-25 12:54:48 -0500

    Debugging

--- src/Photos.js
+++ src/Photos.js
@@ -10,8 +10,8 @@ export default function Photos(props) {
                 {props.photos.slice(3).map(function(photo, index) {
                     return (
                     <div className="col-4 mt-2 mb-2" key={index}>
-                        <a href={photo.src.original} alt={photo.alt} target="_blank" rel="noreferrer">
-                            <img src={photo.src.landscape} className="img-fluid" />
+                        <a href={photo.src.original}  target="_blank" rel="noreferrer">
+                            <img src={photo.src.landscape} alt={photo.alt} className="img-fluid" />
                         </a>
                     </div> );
                 })}
