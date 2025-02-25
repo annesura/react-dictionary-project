3a5901b7fb76dac640c23baeba2e0ba4f039011b
annesura <annesura00@gmail.com>
2025-02-25 12:45:39 -0500

    Added component for photos
    Used map to loop through image API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [word, getWord] = useState(props.defaultKeyword);
     let [results, getResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         getResults(response.data);
     }
+
+    function handleImageResponse(response) {
+        setPhotos(response.data.photos);
+    }
+
 
     function search() {
         let apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
         let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+
+        let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${word}&key=${apiKey}`
+        axios.get(imageApiUrl).then(handleImageResponse);
     }
 
     function handleSubmit(event){
@@ -49,6 +59,7 @@ export default function Dictionary(props) {
                 </div>
                 </section>
                 <Results results={results} />
+                <Photos photos={photos} />
             </div>
         )
     } else {
---
+++ src/Photos.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+    if(props.photos) {
+        console.log(props.photos);
+        return (
+            <section className="Photos">
+                <div className="row">
+                {props.photos.slice(3).map(function(photo, index) {
+                    return (
+                    <div className="col-4 mt-2 mb-2" key={index}>
+                        <a href={photo.src.original} target="_blank" rel="noreferrer">
+                            <img src={photo.src.landscape} className="img-fluid" />
+                        </a>
+                    </div> );
+                })}
+                </div>
+            </section>
+        );
+    } else {
+        return null;
+    }
+}
